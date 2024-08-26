9ffb65c1605eb4aedfd1f119db7379e15fb831c6
rafaella.britto <rafaella.britto@midway.com.br>
2024-08-26 19:38:59 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default class App extends Component {
+  render () {
+    return (
+      <SafeAreaView>
+        <Text>Aqui vai ficar a tela do Campo Minado</Text>
+      </SafeAreaView>
+    );
+  }
+}
