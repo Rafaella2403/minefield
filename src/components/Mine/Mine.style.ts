8cc4b0c5ea851a3e62455eb25b3cee6f6ee5c770
rafaella.britto <rafaella.britto@midway.com.br>
2024-08-27 22:01:15 -0300

    create Mine component

---
+++ src/components/Mine/Mine.style.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  coreMine: {
+    borderRadius: 10,
+    width: 14,
+    height: 14,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  line: {
+    position: 'absolute',
+    height: 3,
+    width: 20,
+    borderRadius: 3,
+    color: '#000000',
+  },
+});
